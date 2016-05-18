43e67ae04288251cdb19e15beed55cafd8405d8a
Collin Clark <crclark96@gmail.com>
2016-05-17 22:06:10 -0400

    Initial commit

---
+++ app/src/main/java/com/example/zachary/harveysexcellentadventure2/Ep3Highscore.java
@@ -0,0 +1,81 @@
+package com.example.zachary.harveysexcellentadventure2;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+public class Ep3Highscore extends ListActivity {
+    private CommentsDataSource3 datasource;
+    private Bundle extras;
+    private String score;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ep3_highscore);
+
+        Intent i = getIntent();
+        extras = i.getExtras();
+        if (extras != null){
+            score = extras.getString("SCORE");
+        }
+
+        datasource = new CommentsDataSource3(this);
+        datasource.open();
+
+        List<Comment> values = datasource.getAllComments();
+
+        Comment comment = datasource.createComment(score);
+
+        // use the SimpleCursorAdapter to show the
+        // elements in a ListView
+        ArrayAdapter<Comment> adapter = new ArrayAdapter<Comment>(this,
+                android.R.layout.simple_list_item_1, values);
+
+        adapter.add(comment);
+
+        setListAdapter(adapter);
+
+    }
+
+    // Will be called via the onClick attribute
+    // of the buttons in main.xml
+
+//    public void delete(View view) {
+//        @SuppressWarnings("unchecked")
+//        ArrayAdapter<Comment> adapter = (ArrayAdapter<Comment>) getListAdapter();
+//        if (getListAdapter().getCount() > 0) {
+//            Comment comment = (Comment) getListAdapter().getItem(0);
+//            datasource.deleteComment(comment);
+//            adapter.remove(comment);
+//        }
+//        adapter.notifyDataSetChanged();
+//    }
+
+    public void retry(View view){
+        Intent i = new Intent(this, Ep3MainActivity.class);
+        startActivity(i);
+    }
+
+    public void mainMenu(View view){
+        Intent i = new Intent(this, MainMenu.class);
+        startActivity(i);
+    }
+
+    @Override
+    protected void onResume() {
+        datasource.open();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        datasource.close();
+        super.onPause();
+    }
+
+}
---
+++ app/src/main/java/com/example/zachary/harveysexcellentadventure2/GameOver.java
@@ -0,0 +1,78 @@
+package com.example.zachary.harveysexcellentadventure2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class GameOver extends AppCompatActivity {
+    TextView t;
+    int score;
+    Intent i, i2;
+    Bundle extras;
+    String newScore, origin;
+    Button b1,b2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_over);
+
+        i = getIntent();
+        extras = i.getExtras();
+        if (extras != null){
+            score = extras.getInt("Score");
+            origin = extras.getString("Game");
+            newScore = Integer.toString(score);
+
+            t = (TextView) findViewById(R.id.textView);
+            if (newScore != null && t != null)
+                t.setText(newScore);
+        }
+
+        b1 = (Button) findViewById(R.id.button5);
+        b2 = (Button) findViewById(R.id.button6);
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
+    public void onClickRetry(View v){
+        switch (origin) {
+            case "Episode 1":
+                i2 = new Intent(this, Ep1MainActivity.class);
+                break;
+            case "Episode 2":
+                i2 = new Intent(this, Ep2MainActivity.class);
+                break;
+            case "Episode 3":
+                i2 = new Intent(this, Ep3MainActivity.class);
+                break;
+        }
+        startActivity(i2);
+    }
+    public void onClickMainMenu(View v){
+        i2 = new Intent(this, MainMenu.class);
+        startActivity(i2);
+    }
+
+    public void onClickHighscore(View v){
+        switch (origin) {
+            case "Episode 1":
+                i2 = new Intent(this, Ep1Highscore.class);
+                break;
+            case "Episode 2":
+                i2 = new Intent(this, Ep2Highscore.class);
+                break;
+            case "Episode 3":
+                i2 = new Intent(this, Ep3Highscore.class);
+                break;
+        }
+        i2.putExtra("SCORE", newScore);
+        startActivity(i2);
+    }
+
+}
